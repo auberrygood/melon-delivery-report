1e45a6d886c48681b1be355cb614bffb978f6fd0
Aubrey Sanders <aubrey.sandrs@gmail.com>
2021-03-22 01:53:00 +0000

    created delivery_info function

--- produce_summary.py
+++ produce_summary.py
@@ -1,3 +1,20 @@
+'''FUNCTIONS'''
+def delivery_info(the_file):
+    '''single function to call for delivery info, when passing in the day's file''' 
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
+
+
+
 print("Day 1")
 the_file = open("um-deliveries-20140519.txt")
 for line in the_file:
